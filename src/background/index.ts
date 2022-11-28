7163528b2152e70d94cacb5ef6ea5c014f63b82b
Lionel <dev@lionelhorn.com>
2022-11-28 06:01:35 +0100

    refactor: add both wa-sqlite & absurd backend to test

--- src/background/index.ts
+++ src/background/index.ts
@@ -3,8 +3,30 @@ import sqlWasmUrl from "@kikko-land/sql.js/dist/sql-wasm.wasm?url";
 import {migrationsPlugin} from "@kikko-land/migrations-plugin";
 import {sql} from "@kikko-land/boono";
 import {initDbClient} from "@kikko-land/kikko";
+import {absurdWebBackend} from "@kikko-land/absurd-web-backend";
 
-const config = {
+const migrations = migrationsPlugin({
+  migrations: [
+    {
+      up: async (db) => {
+        await db.runQuery(
+          sql`
+                CREATE TABLE IF NOT EXISTS jsonRems (
+                  _id TEXT NOT NULL PRIMARY KEY,
+                  doc TEXT
+                );
+                  `,
+        );
+
+        await db.runQuery(sql`CREATE INDEX jsonRems_id ON jsonRems(_id);`);
+      },
+      id: 1000,
+      name: "createRemJson",
+    },
+  ],
+});
+
+const waSqliteConfig = {
   dbName: "helloWorld",
   dbBackend: waSqliteWebBackend({
     wasmUrl: sqlWasmUrl,
@@ -12,31 +34,21 @@ const config = {
     cacheSize: -10000,
     vfs: "minimal",
   }),
-  plugins: [
-    migrationsPlugin({
-      migrations: [
-        {
-          up: async (db) => {
-            await db.runQuery(
-              sql`
-                CREATE TABLE IF NOT EXISTS jsonRems (
-                  _id TEXT NOT NULL PRIMARY KEY,
-                  doc TEXT
-                );
-                  `,
-            );
+  plugins: [migrations],
+};
 
-            await db.runQuery(sql`CREATE INDEX jsonRems_id ON jsonRems(_id);`);
-          },
-          id: 1000,
-          name: "createRemJson",
-        },
-      ],
-    }),
-  ],
+const absurdConfig = {
+  dbName: "helloWorld",
+  dbBackend: absurdWebBackend({
+    wasmUrl: sqlWasmUrl,
+    pageSize: 64 * 1024,
+    cacheSize: -10000,
+  }),
+  plugins: [migrations],
 };
 
 (async () => {
-  const db = await initDbClient(config);
+    const db = await initDbClient(waSqliteConfig);
+   //const db = await initDbClient(absurdConfig);
 })();
 
